e5cac6d5b601c6b12bdc975b5fe0a709489b1b69
DEVB\andrius.kondratjev <andrius.kondratjev@devbridge.com>
2021-07-31 20:07:21 +0300

    add webpack changes

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,17 @@
-const top = "top";
-const bottom = "bottom";
+import  { red, blue } from "./button-styles";
+import css from "./styles/footer.css"
 
-export { top, bottom };
+console.log(css)
+
+const top = document.createElement("div");
+top.innerText = "Top of the footer";
+top.style  = red;
+const bottom = document.createElement("div");
+bottom.innerText = "Bottom of the footer";
+bottom.style  = blue;
+
+const footer = document.createElement("footer");
+footer.appendChild(top);
+footer.appendChild(bottom);
+
+export default footer ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,31 @@
-module.exports = {
-  mode: "none"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const loadPresets = require("./build-utils/loadPresets");
+
+
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  presets = presets || [];
+  return merge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin(), new MiniCssExtractPlugin()],
+      module: {
+          rules: [
+            {
+              test: /\.css$/,
+              use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+          ]
+      }
+    },
+    modeConfig(mode),
+    loadPresets({ mode, presets })
+  );
 };
